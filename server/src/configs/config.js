5ac55484b660c1cf1611bbea829ac4301315afa5
Shivam Sharma <meshivam81@gmail.com>
2025-04-12 12:57:24 +0530

    feat: setup cloudinary for user profile

--- server/src/configs/config.js
+++ server/src/configs/config.js
@@ -5,4 +5,7 @@ export const config = {
   DB_URI: process.env.DB_URI,
   JWT_SECRET: process.env.JWT_SECRET,
   NODE_ENV: process.env.NODE_ENV,
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 };
